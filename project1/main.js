51f77e8f19a58461fcb684342b6517af4b0da0fe
VandersliceApril-FS <acvanderslice@student.fullsail.edu>
2021-03-24 14:52:34 -0500

    Added the first bit of functionality for the draw names button: currently it just shuffles the names on the list. Ref #2

--- project1/main.js
+++ project1/main.js
@@ -1,8 +1,14 @@
+// keep a local list of participants to manipulate
+let listOfParticipants = [];
+
 function addParticipant() {
     // create a new list item element
     let newListItem = document.createElement("LI");
     // grab input value
     let currentInputValue = document.querySelector('.participantNameInput').value;
+
+    // add this name to an array for quick access when creating matches
+    listOfParticipants.push(currentInputValue);
     // append input value to new list item element
     let t = document.createTextNode(currentInputValue);
     newListItem.appendChild(t);
@@ -12,7 +18,39 @@ function addParticipant() {
     allParticipants.appendChild(newListItem);
     document.querySelector('.participantNameInput').value = "";
 }
+
+// when I use this line, an empty list item is already visible on the page and each name is added after it.
+// so 1. is always blank
 // const addNameButton = document.getElementsByClassName('submitParticipantButton').onclick = addParticipant();
+
+// function to take the list and shuffle the names
+function shuffleList(){
+    for(let i = listOfParticipants.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = listOfParticipants[i]
+        listOfParticipants[i] = listOfParticipants[j];
+        listOfParticipants[j] = temp;
+    }
+    return listOfParticipants;
+}
+
+function pairParticipants(shuffled){
+    let pairs = [];
+    shuffled.map((participant, i) => {
+        let indexToMatchWith = i + 1;
+        if (indexToMatchWith >= shuffled.length) {
+            indexToMatchWith = 0;
+        }
+        pairs.push(`${shuffled[i]} buys for ${shuffled[indexToMatchWith]}.`);
+    });
+    // shuffle the list again so it looks more random.
+    return shuffleList(pairs);
+}
+
+const drawButton = document.getElementById('drawNamesButton').onclick = shuffleList;
+console.log(shuffleList());
+
+
 
 
 
